0a9b3228d4ff923069b312a1efa4d5849922a0be
Ryan A Robinett <rrobinet@mit.edu>
2018-08-11 03:35:33 -0400

    fix from merge

--- as_batch.sh
+++ as_batch.sh
@@ -1,4 +1,4 @@
 #To be run on the TX-E1
 rm -f fog* sparta.txt
 
-sbatch -p normal -e error.txt --job-name=tests.sh  -o log-%j -N 4 tests.sh
+sbatch -p normal -e sparta.txt --job-name=tests.sh  -o fog-%j -N 4 tests.sh
