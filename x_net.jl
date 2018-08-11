99f43d628058af2d865b3dc58d2de6669b86cb69
robbobbinett <rrobinet@mit.edu>
2018-08-11 03:55:20 -0400

    tests passing on single_jul_file

--- x_net.jl
+++ x_net.jl
@@ -31,7 +31,7 @@ function extended_mixed_radix_network(Ns::Array{Array{Int, 1}, 1})
 	return W
 end
 
-function kronecker_emr_netowrk(Ns::Array{Array{Int, 1}, 1}, B::Array{Int, 1}, D::Union{Array{Int, 1}, Void}=nothing)
+function kronecker_emr_network(Ns::Array{Array{Int, 1}, 1}, B::Array{Int, 1}, D::Union{Array{Int, 1}, Void}=nothing)
 	M = sum(length.(Ns))
 	gs = map(x -> gcd(B[x], B[x+1]), 1:M)
 	if D == nothing
@@ -44,19 +44,19 @@ function kronecker_emr_netowrk(Ns::Array{Array{Int, 1}, 1}, B::Array{Int, 1}, D:
 		end
 	end
 
-	preW = extended_mixed_radix_newtork(Ns)
+	preW = extended_mixed_radix_network(Ns)
 
         W = Array{SparseMatrixCSC{Int32, Int32}, 1}()
-        for j in 1:length(W)
+        for j in 1:length(preW)
 		gd = div(gs[j], D[j])
-		Gamma = zeros(Int32, (g[j], g[j]))
-		for k in 0:(d-1)
+		Gamma = zeros(Int32, (gs[j], gs[j]))
+		for k in 0:(D[j]-1)
 			pp = conv.(1:gs[j], k, gd, gs[j])
 			p = Permutation(pp)
 			Gamma += Array(p)
 		end
-		BG = kron(ones(Int64, (B[j], B[j+1]), Gamma)
-                push!(W, kron(BG, W[j]))
+		BG = kron(ones(Int64, (B[j], B[j+1])), Gamma)
+                push!(W, kron(BG, preW[j]))
         end
 
         return W
